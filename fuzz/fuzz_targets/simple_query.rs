8e0e669c1afb76c81aa1ff8f8c813832a6c5c764
parsa-jf <parsafares@gmail.com>
2023-06-03 01:05:22 -0700

    mayhem+harness

---
+++ fuzz/fuzz_targets/simple_query.rs
@@ -0,0 +1,18 @@
+use honggfuzz::fuzz;
+use pgcat::messages::simple_query;
+
+fn main() {
+    // Define the fuzzing loop
+    loop {
+        // Fuzzing input will be provided by Honggfuzz
+        fuzz!(|data: &[u8]| {
+            // Convert the input to a string
+            if let Ok(query) = std::str::from_utf8(data) {
+                // Call the simple_query function with the fuzzed input
+                let _ = simple_query(query);
+                
+
+            }
+        });
+    }
+}
